fe3e93d6250b27c14645717a3c60500b7cbed4e8
Patrick Altaie <patrick.altaie@iconsolutions.com>
2017-06-14 08:46:51 +0100

    Don't need SpringSecurityInitializer (that's only for webapps)

--- olyWS/src/main/java/com/wsp/olympics/ws/config/SpringSecurityInitializer.java
+++
@@ -1,6 +0,0 @@
-package com.wsp.olympics.ws.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-}
